b1d9643047e74e66b4d38e2ff397407631e29ccf
Thiago Santos <thia.mdossantos@gmail.com>
2026-06-08 09:22:40 -0300

    feat(nvim): fzf-lua keymaps for find-files, browse-dir, grep-in-dir

---
+++ nvim/lua/plugins/editor.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "ibhagwan/fzf-lua",
+    keys = {
+      {
+        "<leader><leader>",
+        function()
+          local fzf = require("fzf-lua")
+          -- Prefer git files; fall back to all files outside a git repo.
+          if vim.fn.isdirectory(".git") == 1 or vim.fn.systemlist("git rev-parse --is-inside-work-tree 2>/dev/null")[1] == "true" then
+            fzf.git_files({ cmd = "git ls-files --exclude-standard --cached --others" })
+          else
+            fzf.files()
+          end
+        end,
+        desc = "Find files (git-aware)",
+      },
+      {
+        "<leader>ff",
+        function()
+          require("fzf-lua").files({ cwd = vim.fn.expand("%:p:h") })
+        end,
+        desc = "Files in current buffer dir",
+      },
+      {
+        "<leader>s.",
+        function()
+          require("fzf-lua").live_grep({ cwd = vim.fn.expand("%:p:h") })
+        end,
+        desc = "Live grep in current buffer dir",
+      },
+    },
+  },
+}
