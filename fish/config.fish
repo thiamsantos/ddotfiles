56d00a1d4a0f2c74a1129359e4a3e10275547e8e
Thiago Santos <thia.mdossantos@gmail.com>
2026-06-08 12:44:18 -0300

    wip

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,11 @@ set -gx EDITOR nvim
 set -gx AWS_REGION eu-west-1
 set -gx SSH_AUTH_SOCK "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
 
+set -gx CLAUDE_CODE_REMOTE_SEND_KEEPALIVES true
+set -gx BUN_CONFIG_HTTP_IDLE_TIMEOUT 300
+set -gx BUN_CONFIG_HTTP_RETRY_COUNT 3
+set -gx NODE_OPTIONS "--dns-result-order=ipv4first"
+
 fish_add_path "/opt/workbrew/bin"
 fish_add_path "/opt/homebrew/opt/libpq/bin"
 fish_add_path "/opt/homebrew/opt/icu4c/bin"
